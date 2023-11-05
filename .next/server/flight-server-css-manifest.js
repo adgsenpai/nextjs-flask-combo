771cb9f639a3ac5c86c1735e1938c74bc06e8d82
adgsenpai <adg@adgstudios.co.za>
2023-11-05 23:18:28 +0200

    First Commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/flybyadgsenpai/Code/nextjs-flask-combo/app/layout.tsx": [
+      "/Users/flybyadgsenpai/Code/nextjs-flask-combo/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/flybyadgsenpai/Code/nextjs-flask-combo/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/flybyadgsenpai/Code/nextjs-flask-combo/app/page": [
+      "/Users/flybyadgsenpai/Code/nextjs-flask-combo/app/globals.css",
+      "/Users/flybyadgsenpai/Code/nextjs-flask-combo/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
